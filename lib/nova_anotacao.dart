2d6f4c55de9f72540e4b3b20e2f2a55b4cd4f45c
adson62 <adson.matheus62@gmail.com>
2022-01-27 13:52:19 -0300

    layout da tela de adicionar anotacao

--- lib/nova_anotacao.dart
+++ lib/nova_anotacao.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
-class AddAnotacao extends StatelessWidget {
+class AddAnotacao extends StatefulWidget {
   const AddAnotacao({Key? key}) : super(key: key);
 
   @override
+  _AddAnotacaoState createState() => _AddAnotacaoState();
+}
+
+class _AddAnotacaoState extends State<AddAnotacao> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Adicionar Anotação'),
+        appBar: AppBar(
+          title: const Text('Adicionar Anotação'),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Fields(
+                  title: 'Título',
+                  maxLines: 2,
+                ),
+                Container(height: 20.0),
+                const Fields(
+                  title: 'Minha anotação',
+                  maxLines: 10,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class Fields extends StatelessWidget {
+  final String? title;
+  final int? maxLines;
+
+  const Fields({Key? key, this.title, this.maxLines}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      autofocus: true,
+      style: const TextStyle(fontSize: 16),
+      decoration: InputDecoration(
+        labelText: '$title',
+        border: const OutlineInputBorder(),
       ),
-      body: Container(),
     );
   }
 }
