4e2d0c81d101b7236437f37b0350534794ba8afc
adson62 <adson.matheus62@gmail.com>
2022-01-27 16:59:54 -0300

    tela de nova anotacao

--- lib/nova_anotacao.dart
+++ lib/nova_anotacao.dart
@@ -8,52 +8,77 @@ class AddAnotacao extends StatefulWidget {
 }
 
 class _AddAnotacaoState extends State<AddAnotacao> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Adicionar Anotação'),
-        ),
-        body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                const Fields(
-                  title: 'Título',
-                  maxLines: 2,
+      appBar: AppBar(
+        title: const Text('Adicionar Anotação'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Fields(type: 'Título', maxLines: 2),
+              Container(height: 20),
+              Fields(type: 'Anotação', maxLines: 16),
+              Container(height: 20),
+              ElevatedButton(
+                child: const Text('Enviar'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.teal,
+                  elevation: 10,
+                  textStyle: const TextStyle(fontSize: 20),
+                  padding: const EdgeInsets.all(20.0),
                 ),
-                Container(height: 20.0),
-                const Fields(
-                  title: 'Minha anotação',
-                  maxLines: 10,
-                ),
-              ],
-            ),
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+                },
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
 class Fields extends StatelessWidget {
-  final String? title;
+  final String? type;
   final int? maxLines;
+  String? anotacao = '';
 
-  const Fields({Key? key, this.title, this.maxLines}) : super(key: key);
+  Fields({Key? key, this.type, this.maxLines, this.anotacao}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       maxLines: maxLines,
       autofocus: true,
       style: const TextStyle(fontSize: 16),
       decoration: InputDecoration(
-        labelText: '$title',
+        hintText: '$type',
         border: const OutlineInputBorder(),
       ),
+      onChanged: (value) => anotacao = value,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Preencha o(s) campo(s).';
+        }
+        return null;
+      },
     );
   }
 }
