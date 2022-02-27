08b3206270b1f1d970d748775288d3504276bbbc
Adson <adson.matheus62@gmail.com>
2022-02-27 18:23:34 -0300

    mudando variaveis para ingles, add crud em uma unica classe

--- lib/models/anotacao.dart
+++ lib/models/anotacao.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class Anotacao {
+class Note {
   int? id;
   final String titulo;
   final String texto;
   final String dataCriacao;
   int done;
 
-  Anotacao(
+  Note(
       {this.id,
       required this.titulo,
       required this.texto,
@@ -27,66 +27,74 @@ class Anotacao {
   }
 }
 
-Future<Database> databaseCreate() async {
-  WidgetsFlutterBinding.ensureInitialized();
+class CrudNotes extends ChangeNotifier {
+  static CrudNotes instance = CrudNotes();
+  bool created = false;
 
-  final database = openDatabase(
-    join(await getDatabasesPath(), 'anotacoes.db'),
-    onCreate: (db, version) => db.execute(
-        'CREATE TABLE Anotacao (id INTEGER PRIMARY KEY, texto TEXT, titulo TEXT, dataCriacao TEXT, done INTEGER)'),
-    version: 1,
-  );
-  return database;
-}
+  Future<Database> databaseCreate() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-Future<void> createNote(Anotacao anotacao) async {
-  final db = await databaseCreate();
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'anotacoes.db'),
+      onOpen: (db) => db.execute(
+          'CREATE TABLE IF NOT EXISTS Note (id INTEGER PRIMARY KEY, texto TEXT, titulo TEXT, dataCriacao TEXT, done INTEGER)'),
+      version: 1,
+    );
+    return database;
+  }
 
-  db.insert('Anotacao', anotacao.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace);
-}
-
-class Update extends ChangeNotifier {
-  static Update instance = Update();
-
-  Future<void> updateNote(Anotacao anotacao) async {
+  Future<void> createNote(Note note) async {
     final db = await databaseCreate();
 
-    db.update('Anotacao', anotacao.toMap(),
-        where: 'id = ?', whereArgs: [anotacao.id]);
+    db.insert('Note', note.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+
+    instance.created = true;
+  }
+
+  Future<void> updateNote(Note note) async {
+    final db = await databaseCreate();
+
+    db.update('Note', note.toMap(), where: 'id = ?', whereArgs: [note.id]);
     notifyListeners();
   }
-}
 
-Future<List<Map<String, dynamic>>> getNote() async {
-  final db = await databaseCreate();
+  Future<List<Map<String, dynamic>>> getNote() async {
+    final db = await databaseCreate();
 
-  final List<Map<String, dynamic>> notes = await db.query('Anotacao');
+    final List<Map<String, dynamic>> notes = await db.query('Note');
 
-  return notes;
-}
+    return notes;
+  }
 
-Future<List<Map<String, dynamic>>> getNoteById(int id) async {
-  final db = await databaseCreate();
+  Future<List<Map<String, dynamic>>> getNoteById(int id) async {
+    final db = await databaseCreate();
 
-  final List<Map<String, dynamic>> note =
-      await db.query('Anotacao', where: 'id = ?', whereArgs: [id]);
+    final List<Map<String, dynamic>> note =
+        await db.query('Note', where: 'id = ?', whereArgs: [id]);
 
-  return note;
-}
+    return note;
+  }
 
-Future<void> deleteNote(int id) async {
-  final db = await databaseCreate();
+  Future<void> deleteNote(int id) async {
+    final db = await databaseCreate();
 
-  db.delete('Anotacao', where: 'id = ?', whereArgs: [id]);
-}
+    db.delete('Note', where: 'id = ?', whereArgs: [id]);
 
-int isDone(done) {
-  if (done == 1) {
-    return 0;
-  } else if (done == 0) {
-    return 1;
-  } else {
-    return 0;
+    final notes = await getNote();
+    if (notes.isEmpty) {
+      instance.created = false;
+      notifyListeners();
+    }
+  }
+
+  int isDone(done) {
+    if (done == 1) {
+      return 0;
+    } else if (done == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 }
