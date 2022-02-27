3211c5625995c1cbe010d4cadd70e4e772d1e444
Adson <adson.matheus62@gmail.com>
2022-02-27 18:24:16 -0300

    chamando o db na main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:app_anotacoes/principal.dart';
 
 void main() {
-  databaseCreate();
+  CrudNotes.instance.databaseCreate();
   runApp(const Principal());
 }
